28f7c5ccd2ea22f7fa157765366111a02df6a50c
Steve2312 <34347838+Steve2312@users.noreply.github.com>
2020-04-19 11:19:35 +0200

    Test

--- script.js
+++ script.js
@@ -73,18 +73,3 @@ $(document).ready(function() {
 
     imagebanner();
 });
-
-//Preloading Images
-var images = new Array();
-
-function preload() {
-    for (i = 0; i < preload.arguments.length; i++) {
-        images[i] = new Image()
-        images[i].src = preload.arguments[i]
-    }
-}
-
-preload(
-    "images/webready/setup1.png",
-    "images/webready/setup2.png"
-);
