0ab3c7094fe7082cdfa00381b51c577a5ce62b98
Steve2312 <34347838+Steve2312@users.noreply.github.com>
2020-04-19 11:14:43 +0200

    Preloader test

--- script.js
+++ script.js
@@ -73,3 +73,18 @@ $(document).ready(function() {
 
     imagebanner();
 });
+
+//Preloading Images
+var images = new Array();
+
+function preload() {
+    for (i = 0; i < preload.arguments.length; i++) {
+        images[i] = new Image()
+        images[i].src = preload.arguments[i]
+    }
+}
+
+preload(
+    "images/webready/setup1.png",
+    "images/webready/setup2.png"
+);
